3f864d9a039f376e369f5c7ec10f2d5189731904
0Bharat9 <bharatexhash09@gmail.com>
2024-05-25 22:01:02 +0530

    rolling-forecast complete

--- ice_cream/ar-model.py
+++ ice_cream/ar-model.py
@@ -1,9 +1,13 @@
+import pandas as pd
 import numpy as np
+import warnings
 from datetime import timedelta, datetime
 from time import time
 from statsmodels.tsa.arima.model import ARIMA
 from data_cleaning import cleaner
 from plot import production_plot, acf_plot, pacf_plot, plot_error, comparison_plot
+
+warnings.filterwarnings("ignore")
 
 df_ice_cream = cleaner()
 
@@ -12,15 +16,13 @@ acf_plot(df_ice_cream)
 pacf_plot(df_ice_cream)
 
 # based on pacf we can start with an ar(1),ar(2) or ar(3) models
-train_part = datetime(2018, 12, 1)
+train_part = datetime(2016, 12, 1)
 test_part = datetime(2019, 12, 1)
-print(train_part)
-print(test_part)
 
 train_data = df_ice_cream[:train_part]
 test_data = df_ice_cream[train_part + timedelta(days=1):test_part]
 
-model = ARIMA(train_data, order=(13, 0, 0))
+model = ARIMA(train_data, order=(15, 0, 0))
 
 start = time()
 model_fit = model.fit()
@@ -31,13 +33,10 @@ print(model_fit.summary())
 
 pred_start_date = test_data.index[0]
 pred_end_date = test_data.index[-1]
-print(pred_end_date)
-print(pred_start_date)
 preds = model_fit.predict(start=pred_start_date, end=pred_end_date)
-print(preds)
 error = test_data.squeeze() - preds.squeeze()
 
-print(error)
-plot_error(error, 2019, 2021)
-comparison_plot(test_data, preds, 2019, 2020)
+plot_error(error, 2017, 2021)
+comparison_plot(test_data, preds, 2017, 2020)
 print(np.sqrt(np.mean(error**2)))
+
---
+++ ice_cream/rolling_forecast.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import warnings
+from datetime import datetime, timedelta
+from statsmodels.tsa.arima.model import ARIMA
+from data_cleaning import cleaner
+from plot import plot_error, comparison_plot
+
+warnings.filterwarnings("ignore")
+
+
+df_ice_cream = cleaner()
+
+train_part = datetime(2016, 12, 1)
+test_part = datetime(2019, 12, 1)
+
+train_data = df_ice_cream[:train_part]
+test_data = df_ice_cream[train_part + timedelta(days=1):test_part]
+
+# rolling forecast origin
+# train on monthes 1,2,.....,k-2 -> predict month k-1
+# add predicted month into data as it happens and predict the next month so on
+# average all predictions
+df_ice_cream = df_ice_cream.asfreq('MS')
+test_data = test_data.asfreq('MS')
+predictions_rolling = pd.Series(index=test_data.index)
+
+for end_date in test_data.index:
+    train_data = df_ice_cream[:end_date - pd.offsets.MonthEnd(1)]
+    model = ARIMA(train_data, order=(13, 0, 0))
+    model_fit = model.fit()
+    pred = model_fit.predict(end_date)
+    predictions_rolling.loc[end_date] = pred.loc[end_date]
+
+error_rolling = test_data.squeeze() - predictions_rolling.squeeze()
+plot_error(error_rolling, 2017, 2021)
+comparison_plot(test_data, predictions_rolling, 2017, 2020)
+print(np.sqrt(np.mean(error_rolling**2)))
