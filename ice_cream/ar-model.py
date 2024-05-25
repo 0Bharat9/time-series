dfbe56d86a130bc96bbcb49e23ec5f706be8d9d8
0Bharat9 <bharatexhash09@gmail.com>
2024-05-25 13:09:50 +0530

    ar-model for production prediction complete

---
+++ ice_cream/ar-model.py
@@ -0,0 +1,40 @@
+import numpy as np
+from datetime import timedelta, datetime
+from time import time
+from statsmodels.tsa.arima.model import ARIMA
+from data_cleaning import cleaner
+from plot import production_plot, acf_plot, pacf_plot, plot_error, comparison_plot
+
+df_ice_cream = cleaner()
+
+production_plot(df_ice_cream, 'production', 2010, 2021)
+acf_plot(df_ice_cream)
+pacf_plot(df_ice_cream)
+
+# based on pacf we can start with an ar(1),ar(2) or ar(3) models
+train_part = datetime(2018, 12, 1)
+test_part = datetime(2019, 12, 1)
+
+train_data = df_ice_cream[:train_part]
+test_data = df_ice_cream[train_part + timedelta(days=1):test_part]
+
+model = ARIMA(train_data, order=(13, 0, 0))
+
+start = time()
+model_fit = model.fit()
+end = time()
+print('Model fitting time:', end-start)
+
+print(model_fit.summary())
+
+pred_start_date = test_data.index[0]
+pred_end_date = test_data.index[-1]
+
+preds = model_fit.predict(start=pred_start_date, end=pred_end_date)
+
+error = test_data.squeeze() - preds.squeeze()
+
+print(error)
+plot_error(error, 2019, 2021)
+comparison_plot(test_data, preds, 2019, 2020)
+print(np.sqrt(np.mean(error**2)))
--- ice_cream/plot.py
+++ ice_cream/plot.py
@@ -7,15 +7,38 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 df_ice_cream = cleaner()
 
 
-def production_plot(alias):
+def production_plot(alias, label, a, b):
     plt.figure(figsize=(10, 4))
-    plt.plot(alias.production)
+    plt.plot(alias.index, alias[label])
     plt.title('Ice Cream Production over Time', fontsize=20)
     plt.ylabel('Production', fontsize=16)
-    for year in range(2011, 2021):
+    for year in range(a, b):
         plt.axvline(pd.to_datetime(str(year)+'-01-01'),
                     color='k', linestyle='--', alpha=0.2)
 
+    plt.show()
+
+
+def plot_error(alias, a, b):
+    plt.plot(alias, color='red')
+    plt.title('Ice Cream Production over Time', fontsize=20)
+    plt.ylabel('Production', fontsize=16)
+    for year in range(a, b):
+        plt.axvline(pd.to_datetime(str(year)+'-01-01'),
+                    color='k', linestyle='--', alpha=0.2)
+    plt.show()
+
+
+def comparison_plot(alias1, alias2, a, b):
+    plt.figure(figsize=(10, 4))
+    plt.plot(alias1)
+    plt.plot(alias2)
+    plt.legend(('Data', 'Predictions'), fontsize=16)
+    plt.title('Ice cream production overtime', fontsize=20)
+    plt.ylabel('Production', fontsize=16)
+    for year in range(a, b):
+        plt.axvline(pd.to_datetime(str(year)+'-01-01'),
+                    color='k', linestyle='--', alpha=0.2)
     plt.show()
 
 
@@ -25,12 +48,12 @@ def acf_plot(alias):
 
 
 def pacf_plot(alias):
-    plot_pacf(alias.production, lags=50)
+    plot_pacf(alias.production)
     plt.show()
 
 
 if __name__ == "__main__":
-    production_plot(df_ice_cream)
+    production_plot(df_ice_cream, 'production', 2010, 2021)
     # based on decaying ACF, we are likely dealing with an auto regressive process
     acf_plot(df_ice_cream)
     # based on pacf, we should start with an auto regressive model with lags 1,2,3,10,13
